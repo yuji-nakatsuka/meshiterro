74ee9b74ab9ba11d1bb8137ff6d82d6b2269f5a4
yuji-nakatsuka <u.z.aka.dj0ma@gmail.com>
2019-07-14 07:36:39 +0000

    [Finish]5-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,9 +7,12 @@ class PostImagesController < ApplicationController
 	def create
 		@post_image = PostImage.new(post_image_params)
 		@post_image.user_id = current_user.id
-		@post_image.save
+		if @post_image.save
 		redirect_to post_images_path
+	else
+		render :new
 	end
+end
 
 	def index
 		@post_images = PostImage.page(params[:page]).reverse_order
